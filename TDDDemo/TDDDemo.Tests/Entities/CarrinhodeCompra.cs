ab0a5ca13ecd61be1dda7869b10bc0769426d99a
Luís Henrique Rodrigues da Silva <lh.sistemas@hotmail.com>
2020-05-27 17:24:37 -0300

    Adicionando projeto de teste

---
+++ TDDDemo/TDDDemo.Tests/Entities/CarrinhodeCompra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDDemo.Tests.Entities
+{
+    public class CarrinhodeCompra
+    {
+        public CarrinhodeCompra()
+        {
+            itens = new List<Produto>();
+        }
+        public IReadOnlyCollection<Produto> Itens { get => itens; }
+        private List<Produto> itens { get; set; }
+
+        public bool Add(Produto item)
+        {
+            //REGRAS
+            itens.Add(item);
+            return true;
+        }
+    }
+}
